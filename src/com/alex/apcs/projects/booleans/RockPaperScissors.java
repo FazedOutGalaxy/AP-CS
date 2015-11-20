8e11886315739e18de5d9b1059505c5913e88d4b
Alex Chiang <agentleader1@gmail.com>
2015-11-20 02:45:26 -0600

    Finished Rock, Paper, Scissors Lab.
    I finished the Rock, Paper, Scissors lab. Now you can feel free to play
    Rock Paper Scissors with an AI. (:

--- src/com/alex/apcs/projects/booleans/RockPaperScissors.java
+++ src/com/alex/apcs/projects/booleans/RockPaperScissors.java
@@ -1,5 +1,7 @@
 package com.alex.apcs.projects.booleans;
 
+import com.alex.apcs.utils.random.UtilRandomNumbers;
+
 /**
  * 
  * @author Alex Chiang <agentleader1@gmail.com>
@@ -11,25 +13,96 @@ public class RockPaperScissors {
 	private String compChoice;
 
 	public RockPaperScissors() {
-		
+		setPlayers(getRandomChoice());
 	}
 
 	public RockPaperScissors(String player) {
-		
+		setPlayers(player);
 	}
 
-	public void setPlayers(String player) {
-		
+	public void setPlayers(String playChoice) {
+		if (isValid(playChoice))
+			this.playChoice = playChoice;
+		else
+			this.playChoice = getRandomChoice();
+		this.compChoice = getRandomChoice();
 	}
 
 	public String determineWinner() {
-		String winner = "";
-		return winner;
+		boolean playerWin = true;
+		boolean tie = false;
+		String reason = "";
+		if (playChoice.equals("Rock")) {
+			if (compChoice.equals("Rock")) {
+				playerWin = false;
+				tie = true;
+			} else if (compChoice.equals("Paper")) {
+				playerWin = false;
+				tie = false;
+				reason = "Paper Covers Rock";
+			} else if (compChoice.equals("Scissors")) {
+				playerWin = false;
+				tie = false;
+				reason = "Rock Breaks Scissors";
+			}
+		} else if (playChoice.equals("Paper")) {
+			if (compChoice.equals("Rock")) {
+				playerWin = true;
+				tie = false;
+				reason = "Paper Covers Rock";
+			} else if (compChoice.equals("Paper")) {
+				playerWin = false;
+				tie = true;
+			} else if (compChoice.equals("Scissors")) {
+				playerWin = false;
+				tie = false;
+				reason = "Scissors Cuts Paper";
+			}
+		} else {
+			if (compChoice.equals("Rock")) {
+				playerWin = false;
+				tie = false;
+				reason = "Rock Breaks Scissors";
+			} else if (compChoice.equals("Paper")) {
+				playerWin = true;
+				tie = false;
+				reason = "Scissors Cuts Paper";
+			} else if (compChoice.equals("Scissors")) {
+				playerWin = false;
+				tie = true;
+			}
+		}
+		if (playerWin) {
+			return "!Player wins" + " <<" + reason + ">>" + "!";
+		} else {
+			if (tie)
+				return "!Draw Game!";
+			else
+				return "!Computer wins" + " <<" + reason + ">>" + "!";
+		}
 	}
 
 	@Override
 	public String toString() {
-		String output = "";
+		String output = "player had " + playChoice.charAt(0) + "\n" + "computer had " + compChoice.charAt(0) + "\n" + determineWinner();
 		return output;
+	}
+	
+	public static String getRandomChoice() {
+		int random = UtilRandomNumbers.getRandomInteger(1, 3);
+		switch (random) {
+		case 1:
+			return "Rock";
+		case 2:
+			return "Paper";
+		case 3:
+			return "Scissors";
+		default:
+			return null;
+		}
+	}
+	
+	public static boolean isValid(String choice) {
+		return choice.equals("Rock") || choice.equals("Paper") || choice.equals("Scissors");
 	}
 }
--- src/com/alex/apcs/projects/booleans/runners/RPSRunner.java
+++ src/com/alex/apcs/projects/booleans/runners/RPSRunner.java
@@ -2,6 +2,7 @@ package com.alex.apcs.projects.booleans.runners;
 
 import java.util.Scanner;
 
+import com.alex.apcs.abstracts.ApcsProject;
 import com.alex.apcs.projects.booleans.RockPaperScissors;
 
 /**
@@ -9,8 +10,47 @@ import com.alex.apcs.projects.booleans.RockPaperScissors;
  * @author Alex Chiang <agentleader1@gmail.com>
  *
  */
-public class RPSRunner {
+public class RPSRunner extends ApcsProject {
+	
+	private static final RPSRunner instance = new RPSRunner();
+	
 	public static void main(String args[]) {
-		
+		instance.execute();
+	}
+	
+	private static final String format(String original) {
+		if (original.equalsIgnoreCase("rock") ||
+				original.equalsIgnoreCase("roc") ||
+				original.equalsIgnoreCase("roq") ||
+				original.equalsIgnoreCase("r")) {
+			return "Rock";
+		} else if (original.equalsIgnoreCase("paper") ||
+				original.equalsIgnoreCase("papper") ||
+				original.equalsIgnoreCase("papa") ||
+				original.equalsIgnoreCase("pape") ||
+				original.equalsIgnoreCase("p")) {
+			return "Paper";
+		} else {
+			return "Scissors";
+		}
+	}
+	
+
+	@Override
+	public void execute() {
+		RockPaperScissors rps = new RockPaperScissors();
+		Scanner scan = new Scanner(System.in);
+		for (;;) {
+			print("Rock-Paper-Scissors - pick your weapon[R,P,S]:: ");
+			String playChoice = scan.nextLine();
+			rps.setPlayers(format(playChoice));
+			printLine(rps);
+			printLine();
+			print("Do you want to play again? ");
+			String gameChoice = scan.nextLine();
+			if (!(gameChoice.toLowerCase().charAt(0) == 'y'))
+				break;
+			printLine();
+		}
 	}
 }
