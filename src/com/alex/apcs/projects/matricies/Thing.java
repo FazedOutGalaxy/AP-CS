817b52ec1ba2ee7617e3696289e753fb3503ac88
Alex Chiang <agentleader1@gmail.com>
2016-03-16 10:09:57 -0500

    -
    -

--- src/com/alex/apcs/projects/matricies/Thing.java
+++ src/com/alex/apcs/projects/matricies/Thing.java
@@ -1,3 +1,5 @@
+package com.alex.apcs.projects.matricies;
+
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
@@ -8,19 +10,49 @@ import java.util.Scanner;
 import static java.lang.System.*;
 
 //make a Thing class that stores the following 3 instance variables
+public class Thing {
+	
 	// make a String to store type
+	private String type;
 	// make a String to store name
+	private String name;
 	// make a double to store size
+	private double size;
 
 	//add a constrcutor
-
+	public Thing() {
+		
+	}
+	
 	//add set methods
-
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public void setSize(double size) {
+		this.size = size;
+	}
+	
 	//add get methods
-
+	public String getName() {
+		return name;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public double getSize() {
+		return size;
+	}
+	
 	//add a toString 
-
-   
-
-
-
+	@Override
+	public String toString() {
+		
+	}
+}
--- src/com/alex/apcs/projects/matricies/TicTacToe.java
+++ src/com/alex/apcs/projects/matricies/TicTacToe.java
@@ -8,26 +8,78 @@ public class TicTacToe {
 	}
 
 	public TicTacToe(String game) {
-		byte a = 0;
-		byte b = 0;
-		for (char c  : game.toCharArray()) {
+		this();
+		byte a = 0, b = 0;
+		for (char c : game.toCharArray()) {
 			mat[a][b] = c;
-			a++;
-			if (a >= 3) {
-				a = 0;
-				b++;
+			b++;
+			if (b >= 3) {
+				b = 0;
+				a++;
 			}
 		}
 	}
 
 	public String getWinner() {
-		return "";
+		if (checkRows('X'))
+			return "X wins horizontally";
+		else if (checkColumns('X'))
+			return "X wins vertically";
+		else if (checkDiagonals('X'))
+			return "X wins diagonally!";
+		else if (checkRows('O'))
+			return "O wins horizontally!";
+		else if (checkColumns('O'))
+			return "O wins vertically!";
+		else if (checkDiagonals('O'))
+			return "O wins diagonally!";
+		else
+			return "cat's game - no winner!";
+	}
+
+	private boolean checkRows(char c) {
+		for (char[] ch : mat) {
+			byte chks = 0;
+			for (char c_ : ch)
+				if (c_ == c)
+					chks++;
+			if (chks >= 3)
+				return true;
+		}
+		return false;
+	}
+
+	private boolean checkColumns(char c) {
+		for (byte a = 0; a < 3; a++) {
+			byte chks = 0;
+			for (byte b = 0; b < 3; b++)
+				if (mat[b][a] == c)
+					chks++;
+			if (chks >= 3)
+				return true;
+		}
+		return false;
+	}
+
+	private boolean checkDiagonals(char c) {
+		if (((mat[0][0] == c) && (mat[1][1] == c) && (mat[2][2] == c))
+				|| ((mat[0][2] == c) && (mat[1][1] == c) && (mat[2][0] == c)))
+			return true;
+		return false;
 	}
 
 	@Override
 	public String toString() {
 		String output = "";
-
-		return output + "\n\n";
+		for (byte a = 0; a < mat.length; a++) {
+			for (byte i = 0; i < mat[a].length; i++) {
+				output += mat[a][i];
+				if (i < (mat[a].length - 1))
+					output += " ";
+			}
+			if (a < (mat.length - 1))
+				output += "\n";
+		}
+		return output + "\n" + getWinner() + "\n\n";
 	}
 }
